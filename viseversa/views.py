9fd32b05f7189b47847ca5900922dbb3a80099cd
Roma2101 <vynohradovroma@gmail.com>
2020-11-20 20:41:39 +0200

    add word count

--- viseversa/views.py
+++ viseversa/views.py
@@ -7,4 +7,5 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	revers_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':revers_text})
+	word_count = len(user_text.split())
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':revers_text, 'numberofword':word_count})
